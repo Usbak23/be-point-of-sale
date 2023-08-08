8a23b79e72d6cc19a0f1fda2067a2a02769af541
Usbak23 <amubarok1998@gmail.com>
2023-08-09 04:24:17 +0700

    update fungsi

--- app/middleware/multer.js
+++ app/middleware/multer.js
@@ -1,24 +1,29 @@
-const multer = require('multer');
+const multer = require("multer");
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, 'public/uploads/');
+    cb(null, "public/uploads/");
   },
   filename: function (req, file, cb) {
     const now = new Date().toISOString();
-    const date = now.replace(/:/g, '_');
-    cb (null, date + file.originalname);
+    const date = now.replace(/:/g, "_");
+    cb(null, date + file.originalname);
   },
 });
 
 const fileFilter = (req, file, cb) => {
-  if (file.mimetype === 'image/jpeg' || file.mimetype === 'image/png') {
+  if (
+    file.mimetype === "image/jpeg" ||
+    file.mimetype === "image/png" ||
+    file.mimetype === "image/jpg" ||
+    file.mimetype === "image/pdf"
+  ) {
     cb(null, true);
   } else {
     //reject file
     cb(
       {
-        message: 'Unsupported file format',
+        message: "Unsupported file format",
       },
       false
     );
@@ -29,7 +34,7 @@ const uploadMiddleware = multer({
   storage: storage,
   limits: {
     fileSize: 3000000,
-},
+  },
   fileFilter: fileFilter,
 });
 
